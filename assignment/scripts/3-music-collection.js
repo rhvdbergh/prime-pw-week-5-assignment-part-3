7b58815ade311572e7cba2d01245f387e7b79d7c
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-09-13 16:56:14 -0500

    adds new record to collection, returns object, and tests

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,20 @@
 console.log('***** Music Collection *****')
 
 const collection = [];
+
+
+function addToCollection(title, artist, yearPublished) {
+  const newRecord = {
+    title,
+    artist,
+    yearPublished
+  }
+  collection.push(newRecord);
+  return newRecord;
+}
+
+let record = addToCollection("The Clouds Are Abrightening", "The Fuzzy Smokes", 2021);
+console.log(`A new record has been added, check it out: ${record.title}, ${record.artist}, ${record.yearPublished}`);
+record = addToCollection("Snacks For Godzilla", "Deaff Platinum", 1987);
+console.log(`A new record has been added, and it's a classic: ${record.title}, ${record.artist}, ${record.yearPublished}`);
+console.log('The complete music collection: ', collection);
